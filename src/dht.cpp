29576c69d21173f2b1e8eb5e865918ba667a3b2e
Tamir <tmelzerb@ucsc.edu>
2022-10-04 20:02:43 -0700

    Updated header file and tested .cpp file functionality

---
+++ src/dht.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "node.h"
+
+int main()
+{
+    Node n0(5);
+    std::cout << n0.getId();
+}
--- src/node.h
+++ src/node.h
@@ -8,52 +8,65 @@
 
 #define BITLENGTH 8
 
-//forward declaration
+// forward declaration
 class Node;
 
-//The following code is just for reference. You can define your own finger table class.
-//Since the index uniquely determines the interval, only the successor needs to be maintained.  
-class FingerTable{
+// The following code is just for reference. You can define your own finger table class.
+// Since the index uniquely determines the interval, only the successor needs to be maintained.
+class FingerTable
+{
 public:
 	/**
 	 * @param nodeId: the id of node hosting the finger table.
 	 */
-	FingerTable(uint8_t nodeId);
-	void set(size_t index, Node* successor){
-		fingerTable_[index] = successor;
+	FingerTable();
+	void set(size_t index, Node *successor)
+	{
+		fingers[index] = successor;
 	}
-	uint8_t get(size_t index) {
-		return fingerTable_[index];
+	Node *get(size_t index)
+	{
+		return fingers[index];
 	}
 	// TODO: complete print function
 	void prettyPrint();
+
 private:
 	uint8_t nodeId_;
-	std::vector<Node*> fingerTable_;
+	std::vector<Node *> fingers;
 };
 
-FingerTable::FingerTable(uint8_t nodeId): nodeId_(nodeId) {
+FingerTable::FingerTable()
+{
 	// According to Chord paper, the finger table starts from index=1
-	fingerTable_.resize(BITLENGTH + 1);
+	fingers.resize(BITLENGTH + 1);
 }
 
-class Node {
+class Node
+{
 public:
-	Node(uint8_t id): id_(id){}
-	//TODO: implement node join function
+	Node(uint8_t id) : id_(id) {}
+	// TODO: implement node join function
 	/**
 	 * @param node: the first node to contact with to initialize join process. If this is the first node to join the Chord network, the parameter is NULL.
 	 */
-	void join(Node* node);
-	//TODO: implement DHT lookup
+	void join(Node *node);
+	// TODO: implement DHT lookup
 	uint8_t find(uint8_t key);
-	//TODO: implement DHT key insertion
+	// TODO: implement DHT key insertion
 	void insert(uint8_t key);
-	//TODO: implement DHT key deletion
+	// TODO: implement DHT key deletion
 	void remove(uint8_t key);
+
+	// Helper functions
+	uint64_t getId()
+	{
+		return id_;
+	}
+
 private:
 	uint64_t id_;
-	FingerTable fingerTable_;
+	FingerTable fingers;
 	std::map<uint8_t, uint8_t> localKeys_;
 };
 
