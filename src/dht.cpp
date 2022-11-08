be45c51be16ea519070eb868d8b39247a12784a2
Tamir <tmelzerb@ucsc.edu>
2022-11-07 22:10:03 -0800

    New implementation of Node and FingerTable class

--- src/dht.cpp
+++ src/dht.cpp
@@ -3,6 +3,11 @@
 
 int main()
 {
-    Node n0(5);
-    std::cout << n0.getId();
+    Node n0 = Node(0);
+    Node n1 = Node(30);
+
+    n0.join(NULL);
+
+    std::cout << "Successor " << n0.getSuccessor() << std::endl;
+    std::cout << "Predecessor " << n0.getPredecessor() << std::endl;
 }
--- src/node.h
+++ src/node.h
@@ -3,71 +3,97 @@
 
 #include <stdint.h>
 #include <map>
+#include <cmath>
 #include <set>
 #include <vector>
 
 #define BITLENGTH 8
+#define OFFSET 1
+#define TABLE_SIZE BITLENGTH + OFFSET
 
-// forward declaration
+//forward declaration
 class Node;
 
-// The following code is just for reference. You can define your own finger table class.
-// Since the index uniquely determines the interval, only the successor needs to be maintained.
-class FingerTable
-{
+//The following code is just for reference. You can define your own finger table class.
+//Since the index uniquely determines the interval, only the successor needs to be maintained.  
+class FingerTable{
 public:
-	/**
-	 * @param nodeId: the id of node hosting the finger table.
-	 */
-	FingerTable();
-	void set(size_t index, Node *successor)
-	{
-		fingers[index] = successor;
+	FingerTable() {}
+
+	FingerTable(int id){
+		start.resize(BITLENGTH + 1);
+		successors.resize(BITLENGTH + 1);
+		
+		nodeId = id;
+		for(int i = 1; i < TABLE_SIZE; i++){
+			start[i] = (id + ((int) pow(2, i-1))) % ((int) pow(2, BITLENGTH));
+		}
 	}
-	Node *get(size_t index)
-	{
-		return fingers[index];
+
+	void setSuccessor(int index, Node* n){
+		successors[index] = n;
 	}
-	// TODO: complete print function
+
+	void setStart(int index, int val){
+		start[index] = val;
+	}
+
+
 	void prettyPrint();
 
 private:
-	uint8_t nodeId_;
-	std::vector<Node *> fingers;
+	int nodeId;
+	std::vector<int> start;
+	std::vector<Node*> successors;
+
 };
 
-FingerTable::FingerTable()
-{
-	// According to Chord paper, the finger table starts from index=1
-	fingers.resize(BITLENGTH + 1);
-}
-
-class Node
-{
+class Node {
 public:
-	Node(uint8_t id) : id_(id) {}
-	// TODO: implement node join function
+	Node(){}
+
+	Node(int id){
+		nodeId = id;
+		fingers = FingerTable(id);
+	}
+	//TODO: implement node join function
 	/**
 	 * @param node: the first node to contact with to initialize join process. If this is the first node to join the Chord network, the parameter is NULL.
 	 */
-	void join(Node *node);
-	// TODO: implement DHT lookup
-	uint8_t find(uint8_t key);
-	// TODO: implement DHT key insertion
-	void insert(uint8_t key);
-	// TODO: implement DHT key deletion
-	void remove(uint8_t key);
+	void join(Node* node){
+		if(!node){
+			predecessor = this;
+			successor = this;
+			for(int i = 1; i < TABLE_SIZE; i++){
+				fingers.setSuccessor(i, this);
+			}
+		} else {
+			// initFingerTable(node);
+			// updateOthers(node);
+			// migrateKeys();
+		}
+	}
+	//TODO: implement DHT lookup
+	int find(int key);
+	//TODO: implement DHT key insertion
+	void insert(int key);
+	//TODO: implement DHT key deletion
+	void remove(int key);
 
-	// Helper functions
-	uint64_t getId()
-	{
-		return id_;
+
+	int getSuccessor(){
+		return successor->nodeId;
 	}
 
+	int getPredecessor(){
+		return predecessor->nodeId;
+	}
 private:
-	uint64_t id_;
+	int nodeId;
 	FingerTable fingers;
-	std::map<uint8_t, uint8_t> localKeys_;
+	Node* successor;
+	Node* predecessor;
+	std::map<int, int> keys;
 };
 
 #endif
