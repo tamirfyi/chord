4098069ce1115e738c6257ead0777edf42fd6f95
Tamir <tmelzerb@ucsc.edu>
2022-11-20 16:17:35 -0800

    Testing example test case

--- src/dht.cpp
+++ src/dht.cpp
@@ -7,28 +7,54 @@ int main()
     Node n0 = Node(0);
     Node n1 = Node(30);
     Node n2 = Node(65);
-    // Node n3 = Node(110);
-    // Node n4 = Node(160);
-    // Node n5 = Node(230);
+    Node n3 = Node(110);
+    Node n4 = Node(160);
+    Node n5 = Node(230);
 
-    n0.join(NULL); // first node joining hash ring
-    n2.join(&n0);
-    // n3.join(&n2);
-    // n4.join(&n3);
-    // n5.join(&n4);
-
-    // n0.printInfo(); // not first node
-    // n1.printInfo();
-    // n2.printInfo();
-    // n3.printInfo();
-    // n4.printInfo();
-    // n5.printInfo();
-
-    n0.insert(10, 1);
-    n0.insert(20, 1);
-    n0.insert(25, 1);
-    n0.insert(35, -1);
-    n0.insert(40, -1);
-
+    // Node joins
+    n0.join(NULL);
     n1.join(&n0);
+    n2.join(&n0);
+    n3.join(&n2);
+    n4.join(&n3);
+    n5.join(&n4);
+
+    // Node Finger Tables
+    n0.printInfo();
+    n1.printInfo();
+    n2.printInfo();
+    n3.printInfo();
+    n4.printInfo();
+    n5.printInfo();
+
+    // Key Inserts
+    n0.insert(3, 3);
+    n1.insert(200);
+    n2.insert(123);
+    n3.insert(45, 3);
+    n4.insert(99);
+    n2.insert(60, 10);
+    n0.insert(50, 8);
+    n3.insert(100, 5);
+    n3.insert(101, 4);
+    n3.insert(102, 6);
+    n5.insert(240, 8);
+    n5.insert(250, 10);
+
+    n0.printKeys();
+    n1.printKeys();
+    n2.printKeys();
+    n3.printKeys();
+    n4.printKeys();
+    n5.printKeys();
+
+    Node n6 = Node(100);
+    n6.join(&n2);
+
+    n2.leave();
+    n0.printInfo();
+    n1.printInfo();
+    n0.printKeys();
+    n1.printKeys();
+    n6.printKeys();
 }
